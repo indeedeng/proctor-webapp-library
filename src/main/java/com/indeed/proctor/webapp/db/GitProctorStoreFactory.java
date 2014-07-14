b7eefbc846be89a617b8f8002be323a42c71e84b
John Cheng <jcheng@homeaway.com>
2014-07-14 17:15:58 -0500

    added GitWorkspaceProvider to clean up temp files

--- src/main/java/com/indeed/proctor/webapp/db/GitProctorStoreFactory.java
+++ src/main/java/com/indeed/proctor/webapp/db/GitProctorStoreFactory.java
@@ -45,58 +45,51 @@ public class GitProctorStoreFactory implements StoreFactory {
     }
 
     public ProctorStore getTrunkStore() {
-//        return createStore("/trunk/matrices");
-        return createStore("trunk");
+        return createStore("proctor/git/trunk");
     }
 
     public ProctorStore getQaStore() {
-//        return createStore("/branches/deploy/qa/matrices");
-        return createStore("qa");
+        return createStore("proctor/git/qa");
     }
 
     public ProctorStore getProductionStore() {
-//        return createStore("/branches/deploy/production/matrices");
-        return createStore("production");
+        return createStore("proctor/git/production");
     }
     
-    public ProctorStore createStore(final String branchName) {
+    public ProctorStore createStore(final String relativePath) {
 
-        final File tempDirectory = createTempDirectoryForPath(branchName);
-        System.out.println(tempDirectory.getName());
-        System.out.println(tempDirectory.getAbsolutePath());
+        final File tempDirectory = createTempDirectoryForPath(relativePath);
 
         Preconditions.checkArgument(!CharMatcher.WHITESPACE.matchesAllOf(Strings.nullToEmpty(gitUrl)), "scm.path property cannot be empty");
-//        // TODO (parker) 9/13/12 - sanity check that path + relative path make a valid url
-//        final String fullPath = gitUrl + branchName;
-//        System.out.println("fullPath: " + fullPath);
-//
-//        // final gitWorkspaceProviderImpl provider = new gitWorkspaceProviderImpl(tempDirectory, tempDirCleanupAgeMillis);
-        final GitProctor store = new GitProctor(gitUrl, gitUsername, gitPassword, branchName);
+
+        final GitWorkspaceProviderImpl provider = new GitWorkspaceProviderImpl(tempDirectory, tempDirCleanupAgeMillis);
+        final GitProctorCore gitCore = new GitProctorCore(gitUrl, gitUsername, gitPassword, provider);
         
         // actively clean up directories every hour: (not relying on cache eviction)
-        //final long cleanupScheduleMillis = Math.min(TimeUnit.HOURS.toMillis(1), tempDirCleanupAgeMillis);
-        //LOGGER.info("Scheduling SvnWorkspaceProvider every " + cleanupScheduleMillis + " milliseconds for dir: " + tempDirectory + " with age millis " + tempDirCleanupAgeMillis);
-        //executor.scheduleWithFixedDelay(provider, cleanupScheduleMillis, cleanupScheduleMillis, TimeUnit.MILLISECONDS);
+        final long cleanupScheduleMillis = Math.min(TimeUnit.HOURS.toMillis(1), tempDirCleanupAgeMillis);
+        LOGGER.info("Scheduling GitWorkspaceProvider every " + cleanupScheduleMillis + " milliseconds for dir: " +
+                tempDirectory + " with age millis " + tempDirCleanupAgeMillis);
+        executor.scheduleWithFixedDelay(provider, cleanupScheduleMillis, cleanupScheduleMillis, TimeUnit.MILLISECONDS);
 
         if(gitRefreshMillis > 0) {
-            final GitDirectoryRefresher refresher = ((GitProctorCore)store.getCore()).createRefresherTask
+            final GitDirectoryRefresher refresher = gitCore.createRefresherTask
                     (gitUsername, gitPassword);
             LOGGER.info("Scheduling GitDirectoryRefresher every " + gitRefreshMillis + " milliseconds for dir: " + refresher.getDirectoryPath());
             executor.scheduleWithFixedDelay(refresher, gitRefreshMillis, gitRefreshMillis, TimeUnit.MILLISECONDS);
         }
 
-        //final GitProctor store = new GitProctor(gitcore);
+        final String branchName = relativePath.substring(relativePath.lastIndexOf("/")+1);
+        final GitProctor store = new GitProctor(gitCore, branchName);
+        final String prefix = relativePath.replace('/', '-');
         final VarExporter exporter = VarExporter.forNamespace(GitProctor.class.getSimpleName()).includeInGlobal();
-        exporter.export(store, branchName + "-");
+        exporter.export(store, prefix + "-");
 
         return store;
     }
 
     private File createTempDirectoryForPath(final String relativePath) {
         // replace "/" with "-" omit first "/" but omitEmptyStrings
-        System.out.println(relativePath);
         final String dirName = CharMatcher.is(File.separatorChar).trimAndCollapseFrom(relativePath, '-');
-        System.out.println(dirName);
         final File parent = tempRoot != null ? tempRoot : implicitTempRoot;
         final File temp = new File(parent, dirName);
         if(temp.exists()) {
